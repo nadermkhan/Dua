b2d50c8b64556c76a6d374f0874bea6b2f7b0477
nadermkhan <muhammadnadermahbubkhan@gmail.com>
2024-03-13 08:07:16 +0600

    Project Initialized

---
+++ app/src/main/java/nader/islamic/app/dua/MainActivity.java
@@ -0,0 +1,30 @@
+package nader.islamic.app.dua;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import nader.islamic.app.dua.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+	
+	  private ActivityMainBinding binding;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		    binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+		    setSupportActionBar(binding.toolbar);
+
+		    binding.fab.setOnClickListener(v ->
+          Toast.makeText(MainActivity.this, "Replace with your action", Toast.LENGTH_SHORT).show()
+        );
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
+    }
+}
